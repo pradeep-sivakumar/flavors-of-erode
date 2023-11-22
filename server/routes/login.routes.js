7eef1cb7aba1612d11f53c381aa41381286beec5
Pradeep Sivakumar <104561486+pradeep-sivakumar@users.noreply.github.com>
2023-11-22 18:17:01 +0530

    env added

--- server/routes/login.routes.js
+++ server/routes/login.routes.js
@@ -5,32 +5,32 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    // for new user
-    // const newUser = new User({ username, password});
-    // await newUser.save()
-  
-    try {
-      const user = await User.findOne({ username });
-  
-      if (!user) {
-        return res.status(401).json({ userError: 'Invalid username', passwordError : '' });
-      }
-  
-      const isMatch = await bcrypt.compare(password, user.password);
-  
-      if (!isMatch) {
-        return res.status(401).json({ userError:'', passwordError: 'Invalid password' });
-      }
-  
-      const token = jwt.sign({ userId: user._id }, 'eatWiseRestaurant', { expiresIn: '1h' });
-  
-      res.json({ token });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Server error' });
+  // for new user
+  // const newUser = new User({ username, password});
+  // await newUser.save()
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ userError: 'Invalid username', passwordError: '' });
     }
-  });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ userError: '', passwordError: 'Invalid password' });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_PRIVATE_KEY, { expiresIn: '1h' });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 module.exports = router;
